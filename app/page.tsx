b4059c77855736a44b5d008f0843994c93e19785
Prem Kumar <premkumargopinaathan@gmail.com>
2024-07-10 22:16:45 +0530

    navbar complete

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
 import Image from "next/image";
 import Navbar from "../components/Navbar";
-import Footer from "../components/Footer";
+// import Footer from "../components/Footer"
 
 export default function Home() {
   return (
-    <>
+    <div className="bg-gray-700">
       <Navbar />
-      <Footer />
-    </>
+      {/* <Footer /> */}
+    </div>
   );  
 }
